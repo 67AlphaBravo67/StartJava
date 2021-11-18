2d0974c8a6115a818f3a435b16ea4e09584c5a63
67AlphaBravo67 <93768106+67AlphaBravo67@users.noreply.github.com>
2021-11-18 20:28:37 +0300

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args)  {
+        byte numCores = 4;
+        char cpu = 'r';
+        boolean isCompIsWork = true;
+        short cacheMemory = 64;
+        long frequencyHz = 30000000000l;
+        int hddMBytes = 1000000;
+        float price = 86365.55f;
+        double freqMemoryHz = 660000000;
+        System.out.println(numCores);
+        System.out.println(CPU);
+        System.out.println(isCompIsWork);
+        System.out.println(cacheMemory);
+        System.out.println(frequencyHz);
+        System.out.println(hddMegaBytes);
+        System.out.println(price);
+        System.out.println(freqMemoryHz);
+    }
+}
