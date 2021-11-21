c0497271fac19f5c8d5b1cb1817d4ce8055b4ee9
67AlphaBravo67 <lovecars527@gmail.com>
2021-11-21 14:43:26 +0300

    correction again

--- Variables.java
+++ Variables.java
@@ -1,20 +1,20 @@
 public class Variables {
     public static void main(String[] args) {
         byte numCores = 4;
-        System.out.println(numCores);
+        System.out.println("Количество ядер процессора: " + numCores);
         char cpu = 'r';
-        System.out.println(cpu);
+        System.out.println("Модель процессора: " + cpu);
         boolean isCompWork = true;
-        System.out.println(isCompWork);
+        System.out.println( isCompWork);
         short cacheMemory = 64;
-        System.out.println(cacheMemory);
+        System.out.println("Значение кэш-памяти: " + cacheMemory);
         long frequencyHz = 30000000000l;
-        System.out.println(frequencyHz);
+        System.out.println("Приблизительная тактовая частота процессора: " + frequencyHz);
         int hddMBytes = 1000000;
-        System.out.println(hddMegaBytes);
+        System.out.println("Объем памяти жёсткого диска: " + hddMegaBytes);
         float price = 86365.55f;
-        System.out.println(price);
+        System.out.println("Стоимость: " + price);
         double freqMemoryHz = 660000000;
-        System.out.println(freqMemoryHz);
+        System.out.println("Частота оперативной памяти: " + freqMemoryHz);
     }
 }
