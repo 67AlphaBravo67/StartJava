1f65f2d0ce10519291dc79cdad72c6f8d0ad886d
67AlphaBravo67 <lovecars527@gmail.com>
2021-12-09 22:48:20 +0300

    update

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Введите имя первого игрока");
-    Player player1 = new Player(sc.next());
-    System.out.println("Имя первого игрока:" + player1.getName());
-    System.out.println("Введите имя второго игрока");
-    Player player2 = new Player(sc.next());
-    System.out.println("Имя второго игрока: " + player2.getName());
+        Scanner sc = new Scanner(System.in);
+        String = "";
+        while (!answer.equals("no")) {
+            System.out.println("Введите имя первого игрока");
+            Player player1 = new Player(sc.next());
+            System.out.println("Имя первого игрока: " + player1.getName());
+            System.out.println("Введите имя второго игрока");
+            Player player2 = new Player(sc.next());
+            System.out.println("Имя второго игрока: " + player2.getName());
+            GuessNumber game = new GuessNumber(player1, player2);
+            game.compare();
+            do {
+                System.out.println("Продолжить вычисления?")
+                answer = sc.next();
+            } while (!answer.equals("no") && !answer.equals("yes"));
+        }
     }
 }
