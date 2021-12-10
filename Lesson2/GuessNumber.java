6ee59d75285cf8a5a24670cf6b6508fdfd9b6848
67AlphaBravo67 <93768106+67AlphaBravo67@users.noreply.github.com>
2021-12-10 06:44:43 +0300

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -15,16 +15,14 @@ public class GuessNumber {
         Scanner scanner = new Scanner(System.in);
         rand = (int)(0 + (Math.random() * 100) + 1);       
         System.out.println(player1.getName() + ", введите угадываемое число");
-        player1.setNumber(scanner.nextInt());
-        
+        player1.setNumber(scanner.nextInt());        
         if (rand == player1.getNumber()) {
             System.out.println("Поздравляю, число " + player1.getNumber() + " угадано, выиграл первый игрок");
             } else if (rand > player1.getNumber()) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
             } else {
                 System.out.println("Данное число больше того, что загадал компьютер");
-            }
-        
+            }       
         System.out.println(player2.getName() + ", введите угадываемое число");
         player2.setNumber(scanner.nextInt());
         if (rand == player2.getNumber()) {
@@ -35,6 +33,5 @@ public class GuessNumber {
                 System.out.println("Данное число больше того, что загадал компьютер");
             }
         System.out.println("Загаданное число компьютером: " + rand);
-
     }
 }
