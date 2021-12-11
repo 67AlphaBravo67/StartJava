1d2176d030b7afc814e6e0dd218f8f2f236082da
67AlphaBravo67 <lovecars527@gmail.com>
2021-12-11 23:52:47 +0300

    update'

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -17,19 +17,28 @@ public class GuessNumber {
         targetNumber = (int)((Math.random() * 100) + 1);
         System.out.println("Случайное число(Для проверки) " + targetNumber);
         do {
-            numberOfAttempts++;
                 System.out.println("Число, введенное первым игроком.");
+                numberOfAttempts++;
                 player1.setNumber(scanner.nextInt());
                 if (player1.getNumber() == targetNumber) {
                     System.out.println("Выиграл первый игрок");
                     break;
-                }
-                    System.out.println("Число, введенное вторым игроком.");
-                    player2.setNumber(scanner.nextInt());
-                    if (player2.getNumber() == targetNumber) {
+                    } else if (player1.getNumber() > targetNumber) {
+                            System.out.println("Число больше загаданного");
+                    }else if (player1.getNumber() < targetNumber) {
+                            System.out.println("Число меньше загаданного");
+                    }
+
+                System.out.println("Число, введенное вторым игроком.");
+                player2.setNumber(scanner.nextInt());
+                if (player2.getNumber() == targetNumber) {
                     System.out.println("Выиграл второй игрок");
                     break;
-                }
+                    } else if (player2.getNumber() > targetNumber) {
+                            System.out.println("Число больше загаданного");
+                    }else if (player2.getNumber() < targetNumber) {
+                            System.out.println("Число меньше загаданного");
+                    }
         } while ((player1.getNumber() != targetNumber) || (player2.getNumber() != targetNumber));
         System.out.println("Общее число попыток " + numberOfAttempts);
     }
