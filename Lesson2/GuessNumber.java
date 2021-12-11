5232370db893b83039223bc4718d47d78f7d26f4
67AlphaBravo67 <lovecars527@gmail.com>
2021-12-11 17:06:38 +0300

    update

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -15,31 +15,34 @@ public class GuessNumber {
     public void launch() {
         targetNumber = 0.0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое для угадывания число");
+        System.out.println("Введите первое для угадывания число.");
         player1.setNumber(scanner.nextInt());
-        System.out.println("Введите второе для угадывания число");
+        System.out.println("Введите второе для угадывания число.");
         player2.setNumber(scanner.nextInt());
         do {
             numberOfAttempts += 1;
             targetNumber = (int)((Math.random() * 100) + 1);
                 if (player1.getNumber() == targetNumber) {
                     System.out.println("Поздравляю! Число угадано, выиграл первый игрок");            
-                        //break;
+                        break;
                     } else if (player2.getNumber() == targetNumber) {
                     System.out.println("Поздравляю! Число угадано, выиграл второй игрок");
-                        //break; 
+                        break;
                 }               
                 if (player1.getNumber() < targetNumber) {
-                    System.out.println("Данное число меньше загаданного компьютером");
+                    System.out.println("Данное число меньше загаданного компьютером " + (int)(targetNumber));
                 } else if ((player2.getNumber() < targetNumber)) {
-                    System.out.println("Данное число меньше загаданного компьютером");
+                    System.out.println("Данное число меньше загаданного компьютером " + (int)(targetNumber));
                 } else if (player1.getNumber() > targetNumber) {
-                    System.out.println("Данное число больше загаданного компьютером");
+                    System.out.println("Данное число больше загаданного компьютером " + (int)(targetNumber));
                 } else if ((player2.getNumber() > targetNumber)) {
-                    System.out.println("Данное число больше загаданного компьютером");
+                    System.out.println("Данное число больше загаданного компьютером " + (int)(targetNumber));
                 }
         } while ((player1.getNumber() != targetNumber) || (player2.getNumber() != targetNumber));
-        System.out.println("Проверьте себя, компьютер загадал число:" + targetNumber);
+        System.out.println("ПРОВЕРКА");
+            System.out.println("Компьютер загадал число:" + (int)(targetNumber));
+                System.out.println("Первый игрок загадал число:" + (int) (player1.getNumber()));
+            System.out.println("Второй игрок загадал число:" + (int) (player2.getNumber()));
         System.out.println("Общее количество попыток:" + numberOfAttempts);
     }
 }
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -5,10 +5,10 @@ public class GuessNumberTest {
         String answer = "";
         while (!answer.equals("no")) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Введите имя первого игрока");
+            System.out.println("Введите имя первого игрока.");
             Player player1 = new Player(sc.next());
             System.out.println("Имя первого игрока: " + player1.getName());
-            System.out.println("Введите имя второго игрока");
+            System.out.println("Введите имя второго игрока.");
             Player player2 = new Player(sc.next());
             System.out.println("Имя второго игрока: " + player2.getName());
             GuessNumber game = new GuessNumber(player1, player2);
