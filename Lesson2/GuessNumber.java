3621d9fc7815c0789128512202432a556f457060
67AlphaBravo67 <lovecars527@gmail.com>
2021-12-12 12:49:32 +0300

    update

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -26,30 +26,30 @@ public class GuessNumber {
             if (player1.getNumber() == targetNumber) {
                 System.out.println("Выиграл первый игрок по имени " + player1.getName());
                 break;
-                } else if (player1.getNumber() > targetNumber) {
-                        System.out.println("Число больше загаданного.");
-                } else if (player1.getNumber() < targetNumber) {
-                        System.out.println("Число меньше загаданного.");
-                }
+            } else if (player1.getNumber() > targetNumber) {
+                System.out.println("Число больше загаданного.");
+            } else if (player1.getNumber() < targetNumber) {
+                System.out.println("Число меньше загаданного.");
+            }
             System.out.println(player2.getName() + ", введите число.");
             player2.setNumber(scanner.nextInt());
             if (player2.getNumber() == targetNumber) {
                 System.out.println("Выиграл второй игрок по имени " + player2.getName());
                 break;
-                } else if (player2.getNumber() > targetNumber) {
-                        System.out.println("Число больше загаданного.");
-                } else if (player2.getNumber() < targetNumber) {
-                        System.out.println("Число меньше загаданного.");
-                }
+            } else if (player2.getNumber() > targetNumber) {
+                    System.out.println("Число больше загаданного.");
+            } else if (player2.getNumber() < targetNumber) {
+                System.out.println("Число меньше загаданного.");
+            }
             System.out.println(player3.getName() + ", введите число.");
             if (player3.getNumber() == targetNumber) {
                 System.out.println("Выиграл третий игрок по имени " + player3.getName());
                 break;
-                } else if (player3.getNumber() > targetNumber) {
-                        System.out.println("Число больше загаданного.");
-                } else if (player3.getNumber() < targetNumber) {
-                        System.out.println("Число меньше загаданного.");
-                }
+            } else if (player3.getNumber() > targetNumber) {
+                System.out.println("Число больше загаданного.");
+            } else if (player3.getNumber() < targetNumber) {
+                System.out.println("Число меньше загаданного.");
+            }
         } while (true);
         System.out.println("Общее число попыток " + numberOfAttempts);
     }
